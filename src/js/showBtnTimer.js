26439df7f9fdeffc2619fad1aef92d8b826903fe
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2025-06-12 11:27:08 +0300

    upd

---
+++ src/js/showBtnTimer.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.getElementById('showBtn').style.display = 'block';
+  }, 15000); // 20000 миллисекунд = 20 секунд
+});
